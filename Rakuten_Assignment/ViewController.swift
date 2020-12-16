2f1723862afcd222c2be536e26c3dc7759f4b262
srikanth <pandeti.srikanth@wibmo.com>
2020-12-16 10:00:27 +0530

    Initial Commit

---
+++ Rakuten_Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rakuten_Assignment
+//
+//  Created by Srikanth on 16/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
